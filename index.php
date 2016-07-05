0ebf6a35e1a3e15494dc40678afbb368d69778b3
Greg Raven <greg@corax.org>
2016-07-05 06:06:08 -0700

    new favicon

--- index.php
+++ index.php
@@ -1,58 +1,88 @@
 <!DOCTYPE html>
-<html lang="en-US"><!-- InstanceBegin template="/Templates/php-with-bootstrap.dwt" codeOutsideHTMLIsLocked="false" -->
+<html lang="en-US">
+<!-- InstanceBegin template="/Templates/php-with-bootstrap.dwt" codeOutsideHTMLIsLocked="false" -->
 <head>
-<meta charset="utf-8">
+	<meta charset="utf-8">
 <?php include $_SERVER['DOCUMENT_ROOT'] . '/_assets/includes/meta.htm'; ?>
 <!-- InstanceBeginEditable name="doctitle" -->
-<title>The Other Side</title>
+	<title>The Other Side</title>
 <!-- InstanceEndEditable -->
 <!-- InstanceBeginEditable name="head" -->
+	<link rel="apple-touch-icon" sizes="180x180" href="/_assets/favicons/apple-touch-icon.png">
+	<link rel="icon" type="image/png" href="/_assets/favicons/favicon-32x32.png" sizes="32x32">
+	<link rel="icon" type="image/png" href="/_assets/favicons/favicon-16x16.png" sizes="16x16">
+	<link rel="manifest" href="/_assets/favicons/manifest.json">
+	<link rel="mask-icon" href="/_assets/favicons/safari-pinned-tab.svg" color="#5bbad5">
+	<link rel="shortcut icon" href="/_assets/favicons/favicon.ico">
+	<meta name="msapplication-config" content="/_assets/favicons/browserconfig.xml">
+	<meta name="theme-color" content="#ffffff">
 <!-- InstanceEndEditable -->
 </head>
 <body>
 <?php include $_SERVER['DOCUMENT_ROOT'] . '/_assets/includes/navbar.htm'; ?>
 <div class="container-fluid">
-  <header class="page-header"> <!-- InstanceBeginEditable name="page-header" -->
-    <h1> You&rsquo;ve reached the Other Side </h1>
-    <p class="lead"> <strong>Caution:</strong> Fudd&rsquo;s First Law of Opposition strictly observed on these pages. </p>
-    <!-- InstanceEndEditable --> </header>
-  <div class="row">
-    <div class="col-md-12"> <!-- InstanceBeginEditable name="Body" -->
-      <div class="row">
-        <div class="col-md-12">
-          <figure> <a href="/greg-tim-lightning.php"> <img src="/_assets/img/Greg_and_P-38-tn.jpg" alt="Greg Raven the 30th Anniversary Lightning P-38" width="752" height="500" class="img-thumbnail img-responsive"></a>
-            <figcaption> The <a href="http://www.lightningbikes.com/p38/index.html">Lightning P-38</a> and friend </figcaption>
-          </figure>
-        </div>
-
-        <!-- /.span12 -->
-      </div>
-      <!-- /. -->
-      <div class="row">
-        <div class="col-md-12">
-          <div class="well">
-            <h2> My name is Greg Raven &hellip; </h2>
-            <p> Thanks for visiting my web space. I don&rsquo;t have much time to update this website, because I&rsquo;m so busy hunting the ever-slippery Obama bin Lyin&rsquo;: He&rsquo;s here, he&rsquo;s there, he&rsquo;s everywhere &mdash; so beware! </p>
-            <p> You are welcome, of course, to anything you find here. If you want more, check the various links on the &ldquo;<a href="/About_the_Other_Side.php">About</a>&rdquo; page to visit some of my other sites. </p>
-          </div>
-
-          <!-- /.well -->
-        </div>
-        <!-- /. -->
-      </div>
-      <!-- /.span12 -->
-      <!-- InstanceEndEditable --> </div>
-    <!-- /.col-md-12 -->
-  </div>
-  <!-- /.row -->
-  <hr>
+	<header class="page-header">
+<!-- InstanceBeginEditable name="page-header" -->
+		<h1>
+			You&rsquo;ve reached the Other Side
+		</h1>
+		<p class="lead">
+			<strong>Caution:</strong> Fudd&rsquo;s First Law of Opposition strictly observed on these pages.
+		</p>
+<!-- InstanceEndEditable -->
+	</header>
+	<div class="row">
+		<div class="col-md-12">
+<!-- InstanceBeginEditable name="Body" -->
+			<div class="row">
+				<div class="col-md-12">
+					<figure>
+						<a href="/greg-tim-lightning.php"> <img src="/_assets/img/Greg_and_P-38-tn.jpg" alt="Greg Raven the 30th Anniversary Lightning P-38" width="752" height="500" class="img-thumbnail img-responsive"></a>
+						<figcaption>
+							The <a href="http://www.lightningbikes.com/p38/index.html">Lightning P-38</a> and friend
+						</figcaption>
+					</figure>
+				</div>
+<!-- /.span12 -->
+			</div>
+<!-- /. -->
+			<div class="row">
+				<div class="col-md-12">
+					<div class="well">
+						<h2>
+							My name is Greg Raven &hellip;
+						</h2>
+						<p>
+							Thanks for visiting my web space. I don&rsquo;t have much time to update this website, because I&rsquo;m so busy hunting the ever-slippery Obama bin Lyin&rsquo;: He&rsquo;s here, he&rsquo;s there, he&rsquo;s everywhere &mdash; so beware!
+						</p>
+						<p>
+							You are welcome, of course, to anything you find here. If you want more, check the various links on the &ldquo;<a href="/About_the_Other_Side.php">About</a>&rdquo; page to visit some of my other sites.
+						</p>
+					</div>
+<!-- /.well -->
+				</div>
+<!-- /. -->
+			</div>
+<!-- /.span12 -->
+<!-- InstanceEndEditable -->
+		</div>
+<!-- /.col-md-12 -->
+	</div>
+<!-- /.row -->
+	<hr>
 </div>
 <!-- /.container -->
 <?php include $_SERVER['DOCUMENT_ROOT'] . '/_assets/includes/footer.htm'; ?>
-<!-- InstanceBeginEditable name="bottom-js" --><!-- #BeginLibraryItem "/Library/active-home.lbi" --><script>
-  $(document).ready(function () {
-    $('#home').addClass('active');
-  });
-</script><!-- #EndLibraryItem --><!-- InstanceEndEditable -->
+<!-- InstanceBeginEditable name="bottom-js" -->
+<!-- #BeginLibraryItem "/Library/active-home.lbi" -->
+<script>
+	$(document).ready(function () {
+	  $('#home').addClass('active');
+	});
+</script>
+
+<!-- #EndLibraryItem -->
+<!-- InstanceEndEditable -->
 </body>
-<!-- InstanceEnd --></html>
+<!-- InstanceEnd -->
+</html>
